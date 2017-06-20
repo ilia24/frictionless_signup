6ee6821df92dce71e7159bbbf920eb2dde047325
ilia24 <iliademertchian24@gmail.com>
2017-06-20 03:12:11 -0400

    Change database to match assignment

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -12,9 +12,8 @@ $(document).on('turbolinks:load', function() {
       dataType: 'JSON',
       url: 'https://person-stream.clearbit.com/v2/combined/find?email=' + $('#email').val()
       }).done(function(data){
-        $('#first_name').val(data.person.name.givenName)
-        $('#last_name').val(data.person.name.familyName)
-
+        $('#full_name').val(data.person.name.fullName)
+        $('#company_name').val(data.company.legalName)
       }).fail(function(){
         console.log('Person not found!');
       });
--- db/migrate/20170619174728_create_users.rb
+++ db/migrate/20170619174728_create_users.rb
@@ -1,10 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
-      t.string :first_name
-      t.string :last_name
-      t.string :role
+      t.string :full_name
       t.string :company_name
+      t.integer :company_size
+      t.string :phone_number
       t.string :password_digest
 
       t.timestamps
