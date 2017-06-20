04a02209476a9587eb3aac04db8485b878532423
ilia24 <iliademertchian24@gmail.com>
2017-06-20 00:32:22 -0400

    Add base routes/controller actions for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,27 @@
 class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:user_id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      render :new
+    else
+      render :new
+    end
+  end
+
+private
+
+  def user_params
+    params.require(:user).permit(:email, :first_name, :last_name, :role, :company_name, :password)
+  end
+
 end
