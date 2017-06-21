06f1c62eb81111691f74e18a2d897bb2b7fe1542
ilia24 <iliademertchian24@gmail.com>
2017-06-21 04:25:27 -0400

    Add parser script to go through meetup group users and scrape names

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,19 +9,43 @@ class ApplicationController < ActionController::Base
   end
 
   def scrape_users
-
     # P1: Takes all the popular meetup groups on the homepage & stashes them in an array
     agent = Mechanize.new
     page = agent.get 'http://www.meetup.com'
     meetup_links = []
+    people = []
 
     page.links.select{|link| link.href[/events/]}.each do |eventlink|
       url = eventlink.href
-      url.gsub!(/(\/events\/).+/, "/members/" )
+      url.gsub!(/(\/events\/).+/, "/members/?offset=0" )
       meetup_links << url
     end
 
+    #P2: Takes each group link, and iterates through all the member pages(limited by user count), scraping each name into my array along the way
+    meetup_links.each do |meetlink|
+      page = HTTParty.get(meetlink)
+      parsed_page = Nokogiri::HTML(page)
+      user_limit = (parsed_page.css('.D_count').first.text.delete"(,)").to_i #gets member count from page, removes styling, and converts to integer
+      offset = 0
 
+      while offset <= user_limit do
+        page = HTTParty.get(meetlink)
+        parsed_page = Nokogiri::HTML(page)
+
+        parsed_page.css('.memName').each do |user|
+          people << user.text
+        end
+
+        offset += 20
+        meetlink.gsub!((offset - 20).to_s, offset.to_s) #gsubs the old offset with the new one (As string to avoid implicit conversion errors)
+
+        if people.length > 100
+          render :json => people and return
+        end
+
+      end
+
+    end
 
   end
 
