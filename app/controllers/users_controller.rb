684391c91a98d0b5248af3059a362c0708b4239e
ilia24 <iliademertchian24@gmail.com>
2017-06-21 13:42:33 -0400

    Remove excess methods, clean up routing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,6 @@
 class UsersController < ApplicationController
   before_action :clearbit_auth, only: [:get_user_info]
 
-  def show
-    @user = User.find(params[:user_id])
-  end
-
   def new
     @user = User.new
   end
@@ -13,7 +9,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      redirect_to root_path
+      redirect_to '/'
     else
       render :new
     end
